1ebb9677ffba06ae10fb6f5cf333f29b28976dc3
Guilherme Augusto <guilhermesaldanha08@gmail.com>
2023-05-18 16:31:23 -0300

    first commit

---
+++ src/main/kotlin/dev/guilherme/requestcreditsystem/RequestCreditSystemApplication.kt
@@ -0,0 +1,11 @@
+package dev.guilherme.requestcreditsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestCreditSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestCreditSystemApplication>(*args)
+}
