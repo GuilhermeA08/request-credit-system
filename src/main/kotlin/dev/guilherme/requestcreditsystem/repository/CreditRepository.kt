6e4e49dc2afc02142c5bc11e65307cbe363df700
Guilherme Augusto <guilhermesaldanha08@gmail.com>
2023-05-22 15:57:21 -0300

    feat: repository

---
+++ src/main/kotlin/dev/guilherme/requestcreditsystem/repository/CreditRepository.kt
@@ -0,0 +1,9 @@
+package dev.guilherme.requestcreditsystem.repository
+
+import dev.guilherme.requestcreditsystem.entity.Credit
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CreditRepository: JpaRepository<Credit, Long> {
+}
