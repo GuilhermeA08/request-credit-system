824f6ffadc6288c283b1fdf2b7ee7ca6c25bef9a
Guilherme Augusto <guilhermesaldanha08@gmail.com>
2023-05-28 16:32:41 -0300

    feat: valid numberojInstallments

--- src/main/kotlin/dev/guilherme/requestcreditsystem/dto/CreditDto.kt
+++ src/main/kotlin/dev/guilherme/requestcreditsystem/dto/CreditDto.kt
@@ -3,6 +3,8 @@ package dev.guilherme.requestcreditsystem.dto
 import dev.guilherme.requestcreditsystem.entity.Credit
 import dev.guilherme.requestcreditsystem.entity.Customer
 import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 import java.time.LocalDate
@@ -12,7 +14,11 @@ data class CreditDto(
     @field:NotNull(message = "Credit value is required")
     val creditValue: BigDecimal,
     @field:Future(message = "Date must be in the future")
+    @field:NotNull(message = "Date is required")
     val dayFirstOfInstallment: LocalDate,
+    @field:NotNull(message = "Number of installments is required")
+    @field:Min(value = 1, message = "Credit value must be greater than 0")
+    @field:Max(value = 48, message = "Credit value must be <= 48")
     val numberOfInstallments: Int,
     @field:NotNull(message = "Customer id is required")
     val customerId: Long
